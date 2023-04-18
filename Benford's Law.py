76317541a6f3e3c765d037edc429869ca32c6171
Nathantambourine <340867852@gapps.yrdsb.ca>
2023-04-18 15:31:06 -0400

    I have made a menu for question 1

--- Benford's Law.py
+++ Benford's Law.py
@@ -1,10 +1,43 @@
+import os
+print(os.getcwd())
 def printMenu():
     print('''
           Customer and Sales System\n
-          1. Enter Customer Information\n
-          2. Generate Customer data file\n
-          3. Report on total Sales (Not done in this part)\n
-          4. Check for fraud in sales data (Not done in this part)\n
-          9. Quit\n
-          Enter menu option (1-9)
+          1. Load sales data\n
+          2. Create Chart\n
+          3. Exit 
+          Enter menu option (1-3)
           ''')
+
+
+        
+       
+
+userInput = ""
+saledata = "1"
+createchart = "2"
+exitCondition = "3"
+
+
+
+
+while userInput != exitCondition:
+    printMenu()               
+    userInput = input();   
+
+    if userInput == saledata:        
+        folder = os.getcwd()
+        fileName = folder + "\\sales.csv"
+        file = open(fileName, "r")
+        print(file.read())
+        file.close()
+
+    elif userInput == createchart: 
+        
+        createchart()
+
+    else:
+        print("Please type in a valid option (A number from 1-3)")
+
+
+print("Program Terminated")
